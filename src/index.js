db5599efe1527c1d6774d8436b7d544d2358f7fa
Komalis <moderationkomalis@gmail.com>
2018-11-25 20:27:18 +0100

    Created a default index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import store from "./store";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {ExampleContainer} from "./containers/Example";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={ExampleContainer}/>
+            </Switch>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
